2f180760615e98c0733805eac5632b6c967dca73
FuadHr <foadh1988@gmail.com>
2022-07-14 17:15:41 +0430

    changes

--- src/main/java/me/samplespring/my_samples/service/ProductService.java
+++ src/main/java/me/samplespring/my_samples/service/ProductService.java
@@ -1,8 +1,15 @@
 package me.samplespring.my_samples.service;
 
 
+import me.samplespring.my_samples.domain.Product;
+import me.samplespring.my_samples.domain.Reservation;
+import me.samplespring.my_samples.domain.User;
+import me.samplespring.my_samples.model.ProductDTO;
+import me.samplespring.my_samples.model.ReservationDTO;
 import me.samplespring.my_samples.repos.ProductRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ProductService {
@@ -10,5 +17,32 @@ public class ProductService {
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public ProductDTO get(final Long productNo){
+        return this.productRepository.findById(productNo).map(product -> mapToDTO(product, new ProductDTO()))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    private ProductDTO mapToDTO(final Product product,
+                                final ProductDTO productDTO) {
+        productDTO.setId(product.getId());
+        productDTO.setProductName(product.getProductName());
+        productDTO.setProductNo(product.getProductNo());
+        //productDTO.setUser(product.getUser() == null ? null : product.getUser());
+        return productDTO;
+    }
+
+    private Product mapToEntity(final ProductDTO productDTO,
+                                    final Product product) {
+        product.setId(productDTO.getId());
+        product.setProductName(productDTO.getProductName());
+        product.setProductNo(productDTO.getProductNo());
+        /*if (productDTO.getUser() != null && (product.getUser() == null || !product.getUser().getId().equals(productDTO.getUser().getId()))) {
+            final User user = userRepository.findById(productDTO.getUser().getId())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found"));
+            product.setUser(user);
+        }*/
+        return product;
     }
 }
